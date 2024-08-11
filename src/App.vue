44cf6be3a41b95590b05f95f5f32dddbc2b38e83
Pevara <pvanraemdonck@telenet.be>
2024-08-11 02:22:08 +0200

    adding material bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <header>
-    <h1>Tools for Builders</h1>
-    <button @click="menuVisible = ! menuVisible">menu</button>
-    <nav v-if="menuVisible">
-      <ul>
-        <li>Triangles</li>
-      </ul>
-    </nav>
-  </header>
-  <router-view />
+  <div v-bind:data-bs-theme="store.theme">
+    <header>
+      <h1>Tools for Builders</h1>
+      <button @click="menuVisible = ! menuVisible">menu</button>
+      <nav v-if="menuVisible">
+        <ul>
+          <li>Triangles</li>
+        </ul>
+      </nav>
+    </header>
+    <router-view />
+  </div>
+
 </template>
 
 <script lang="ts" setup>
@@ -20,24 +23,5 @@
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "~@materialstyle/materialstyle/scss/materialstyle";
 </style>
