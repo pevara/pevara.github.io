8b94f5e98da781a224221c94426cf1c3b8b80eb4
Pevara <pvanraemdonck@telenet.be>
2024-08-11 04:34:36 +0200

    adding i18n support

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,59 @@
-<template>
-  <div>
-    <header class="navbar navbar-expand-xl bg-primary">
-      <div class="container-fluid">
-        <a class="navbar-brand d-flex align-items-center" >
-          <i class="bi bi-star-fill me-2"></i>Tools for Builders
-        </a>
-      </div>
-    </header>
-    <div class="container-fluid p-2">
-
-  <router-view />
-
-</div>
-
-  </div>
-
-</template>
-
 <script lang="ts" setup>
   import {ref} from "vue";
   import {useAppStore} from "@/store/app";
-  import { useHead } from '@unhead/vue';
+  import {useHead} from '@unhead/vue';
+  import { useI18n } from 'vue-i18n';
+  const { t } = useI18n() ;
 
   const store = useAppStore();
   const menuVisible = ref(false);
 
   useHead({
-    title: "tools for builders",
-    htmlAttrs: {
-      "data-bs-theme": store.theme
-    }
+      title: "tools for builders",
+      htmlAttrs: {
+          "data-bs-theme": store.theme
+      }
   });
 </script>
 
+
+<template>
+    <div>
+        <header class="navbar navbar-expand-xl bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand d-flex align-items-center">
+                    <i class="bi bi-star-fill me-2"></i>{{ t("title") }}
+                </a>
+            </div>
+        </header>
+        <div class="container-fluid p-2">
+
+            <router-view/>
+
+        </div>
+
+    </div>
+
+</template>
+
+
 <style lang="scss">
-$primary:#086f91;
-$secondary:#72a7b1;
-$enable-shadows:true;
-$enable-gradients:true;
-$enable-grid-classes:false;
-@import "~@materialstyle/materialstyle/scss/materialstyle";
+  $primary: #086f91;
+  $secondary: #72a7b1;
+  $enable-shadows: true;
+  $enable-gradients: true;
+  $enable-grid-classes: false;
+  @import "~@materialstyle/materialstyle/scss/materialstyle";
 </style>
+
+
+<i18n>
+  {
+    "en": {
+      "title": "Tools for Builders"
+    },
+    "nl": {
+      "title": "Tools voor Bouwers"
+    }
+  }
+</i18n>
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import { createPinia } from "pinia";
-import { createHead } from "@unhead/vue";
+import {createPinia} from "pinia";
+import {createHead} from "@unhead/vue";
+import {createI18n} from 'vue-i18n'
 
 const pinia = createPinia();
 const head = createHead();
+const i18n = createI18n({
+    legacy: false,
+    locale: 'en',
+    messages: {
+      en: {},
+      nl: {}
+    }
+})
 
 createApp(App)
     .use(pinia)
     .use(router)
     .use(head)
+    .use(i18n)
     .mount("#app");
