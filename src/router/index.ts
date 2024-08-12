bf5424581938359b22534ccac5de77e8065f56df
Pevara <pvanraemdonck@telenet.be>
2024-08-12 03:08:54 +0200

    setting up base layout and adding first pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,41 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
+import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
+import PrivacyView from "@/views/PrivacyView.vue";
+import SettingsView from "@/views/SettingsView.vue";
+import TriangleView from "@/views/TriangleView.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    component: AboutView,
-  },
+	{
+		path: "/",
+		name: "home",
+		component: HomeView,
+	},
+	{
+		path: "/about",
+		name: "about",
+		component: AboutView,
+	},
+	{
+		path: "/privacy",
+		name: "privacy",
+		component: PrivacyView,
+	},
+	{
+		path: "/settings",
+		name: "settings",
+		component: SettingsView,
+	},
+	{
+		path: "/triangles",
+		name: "triangles",
+		component: TriangleView,
+	},
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
 });
 
 export default router;
---
+++ src/views/PrivacyView.vue
@@ -0,0 +1,5 @@
+<template>
+  
+    <h1>This is a privacy page</h1>
+
+</template>
