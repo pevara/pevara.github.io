42613834df06f8179d112a39b130f3f627aed42a
Pevara <pvanraemdonck@telenet.be>
2024-08-14 17:01:45 +0200

    clean quasar install

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,36 @@
-import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
-import HomeView from "@/views/HomeView.vue";
-import AboutView from "@/views/AboutView.vue";
-import PrivacyView from "@/views/PrivacyView.vue";
-import SettingsView from "@/views/SettingsView.vue";
-import TriangleView from "@/views/TriangleView.vue";
-import ConversionView from "@/views/ConversionView.vue";
+import { route } from 'quasar/wrappers';
+import {
+  createMemoryHistory,
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from 'vue-router';
 
-const routes: Array<RouteRecordRaw> = [
-	{
-		path: "/",
-		name: "home",
-		component: HomeView,
-	},
-	{
-		path: "/about",
-		name: "about",
-		component: AboutView,
-	},
-	{
-		path: "/privacy",
-		name: "privacy",
-		component: PrivacyView,
-	},
-	{
-		path: "/settings",
-		name: "settings",
-		component: SettingsView,
-	},
-	{
-		path: "/triangles",
-		name: "triangles",
-		component: TriangleView,
-	},
-	{
-        path: '/conversion',
-        name: 'conversion',
-        component: ConversionView
-    },
-];
+import routes from './routes';
 
-const router = createRouter({
-	history: createWebHistory(process.env.BASE_URL),
-	routes,
+/*
+ * If not building with SSR mode, you can
+ * directly export the Router instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Router instance.
+ */
+
+export default route(function (/* { store, ssrContext } */) {
+  const createHistory = process.env.SERVER
+    ? createMemoryHistory
+    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+
+  const Router = createRouter({
+    scrollBehavior: () => ({ left: 0, top: 0 }),
+    routes,
+
+    // Leave this as is and make changes in quasar.conf.js instead!
+    // quasar.conf.js -> build -> vueRouterMode
+    // quasar.conf.js -> build -> publicPath
+    history: createHistory(process.env.VUE_ROUTER_BASE),
+  });
+
+  return Router;
 });
-
-export default router;
--- src/views/SettingsView.vue
+++
@@ -1,3 +0,0 @@
-<template>
-	<h1>This is a settings page</h1>	
-</template>
